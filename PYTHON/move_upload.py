7ddf5a93acbe0f520d7ed5b429423c54a24f2228
FW_IX <borntokill.ix@gmail.com>
2022-07-27 00:01:53 +0700

    2. Added options for custom path and completed option antefreeze

--- PYTHON/move_upload.py
+++ PYTHON/move_upload.py
@@ -7,13 +7,13 @@ import pwd
 import grp
 import re 
 
-def create_dir(dir_name='archive', you_path='/var/lib/staffcop/upload/' ):
+def create_dir(dir_name='archive', path='' ):
     #print dir_name
-    #print you_path
+    #print path
     try:
-        os.chdir(you_path)
+        os.chdir(path)
     except:
-        create_dir(you_path)
+        create_dir(path)
 
     if not os.path.isdir(dir_name):
         os.mkdir(dir_name,0o755)
@@ -55,26 +55,33 @@ class Data_Bases:
 
 
     def __del__(self):
-        self.conn.close()
+        # self.conn.close()
         print "\nDataBase is Closed\n"
 
 class Update:
 
-    def __init__(self, cold_database, def_path='/var/lib/staffcop/upload/', flag=''):
+    def __init__(self, cold_database, def_path='', flag=''):
         self.con = Data_Bases(cold_database)
         #self.atttach_files = self.get_data(select,path)
         self.path = def_path
+
+        # If you want move files from the cold storage
         self.flag = flag
+        self.name_database = cold_database
 
         #Create a full path to coldbase storage like /var/lib/staffcop/upload/staffcop_archive
         if flag is '':
             self.middle_path = self.path + cold_database 
+            self.name_database = ''
+            self.flag_condition = ''
+        # If you want move files from the cold storage
         else :
-            self.middle_path = "filedata/by_date" #If flag is false then the "filedata/" path is default 
+            self.middle_path = 'filedata/by_date' #If flag is false then the "filedata/" path is default 
+            self.start_path = '/var/lib/staffcop/upload/'
+            self.flag_condition = "AND att.data ~ '%s'" % self.name_database
 
 
     def get_data(self, select):
-        count = 0
 
         self.con.request(select)
         self.attach_files = self.con.fetch()
@@ -83,66 +90,79 @@ class Update:
 
     def get_folders(self):
         select_folders = """
-            SELECT DISTINCT substring(att.data from '\d{4}_\d{2}_\d{2}/') 
+            SELECT DISTINCT substring(att.data from '/\d{4}_\d{2}_\d{2}/') 
             FROM agent_event ae 
             LEFT JOIN agent_attachedfile att 
                 ON ae.attached_file_id = att.id 
-            WHERE att.data %s LIKE 'file%%'
-            """ % self.flag
+            WHERE att.data %s LIKE 'file%%' %s
+            """ % (self.flag, self.flag_condition)
 
         self.con.request(select_folders)
         self.folders = self.con.fetch()
         return self.folders
-    
-           
+
+
     #Move on the files
     def move_files(self):
         for folder in self.get_folders():
-            old_path = '{}filedata/by_date/{}'.format(self.path,folder)
-            new_path = '{}/{}'.format(self.middle_path,folder)
+            old_path = '{}{}/{}'.format(self.path,self.name_database, folder)
+            new_path = '{}{}/{}'.format(self.start_path, self.middle_path,folder)
+
             try:
-                #os.renames(old, new)
+                os.renames(old_path, new_path)
                 print 'Directory {} was moved to {}'.format(old_path,new_path)
-            except :
-                print 'Directory {} does not exist!\n'.format(old_path)
+            except Exception as e :
+                print '{} {} !\n'.format(e, old_path)
 
     def update_links(self):
-        #Make absolyte path to file ##/var/lib/staffcop/upload/staffcop2/2022_04_06/c2695b75d0c85acd06d5b4a6c7d53207a20ae7ec.png
-        for clear_file in attach_files :
+        count = 0
+        #Make absolyte path to file ##/var/lib/staffcop/upload/archive_db/2022_04_06/c2695b75d0c85acd06d5b4a6c7d53207a20ae7ec.png
+        for clear_file in self.attach_files :
             new_absolute_path_to_file = '{}/{}'.format(self.middle_path,clear_file) 
 
             #Updating links into DB
             update_query = "UPDATE agent_attachedfile SET data='{}' WHERE data LIKE '%{}';".format(new_absolute_path_to_file,clear_file) #clear_file is search condition
 
-            #con.request(update_query)
-            #print update_query
+            self.con.request(update_query)
+            # print update_query
             count = count + 1
 
-        con.com()
-        con.close_con()
-        print "\n\n{} Links into Database {} Updated!\n".format(count,cold_database)
+        self.con.com()
+        self.con.close_con()
+        print "\n\n{} Links into Database {} Updated!\n".format(count,self.name_database)
 
+# Start code of this
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, 
+        description="""This is the scrip created for updating and move files from Database and filestorage into cold storage 
+        Example : ./move_upload.py --path /mnt/cold_upload""") 
 
-parser = argparse.ArgumentParser(description='Do it!')
-subparsers = parser.add_subparsers(title='subcommands',
-                                   description='valid subcommands',
-                                   help='description')
-get_parser = subparsers.add_parser('')
-#parser.add_argument('--freeze', dest='flag', default='')
-#parser.add_argument('--antifreeze', dest='flag', default='NOT', required=False)
+parser.add_argument('-a', '--antifreeze', action='store_true', required=False, help='Go to back files')
+parser.add_argument('-p', '--path', action='store_true', required=False, help='Move file into specified storage')
 args = parser.parse_args()
-flag = args.flag
+
+if args.path:
+    path = path + '/'
+else:
+    path = '/var/lib/staffcop/upload/'
+
+
+#For move to files from cold storage into default path
+if args.antifreeze:
+    flag = 'NOT'
+else:
+    flag = ''
+
 
 #VARIABLES
 #select_tablespace = "SELECT spcname FROM pg_tablespace WHERE spcname NOT IN ('pg_default', 'pg_global');"
 select_dbname = """
-   SELECT datname 
-   FROM pg_database db 
-   LEFT JOIN pg_tablespace tbsp 
-      ON tbsp.oid=db.dattablespace 
-   WHERE tbsp.spcname 
-   NOT IN ('pg_default','pg_global')
-   """
+    SELECT datname 
+    FROM pg_database db 
+    LEFT JOIN pg_tablespace tbsp 
+       ON tbsp.oid=db.dattablespace 
+    WHERE tbsp.spcname 
+    NOT IN ('pg_default','pg_global')
+    """
 
 select_old_data = """
     SELECT substring(att.data from '\d{4}_\d{2}_\d{2}/\w+\.\w{3,}') 
@@ -159,9 +179,14 @@ con.request(select_dbname)
 db_name = con.fetch()
 con.close_con
 
+# Updating links inside DataBase
 for database in db_name:
-    up = Update(database, flag=flag)
-    data = up.get_data(select_old_data)
-    print up.get_folders()
-    print up.move_files()
-    #print data
+    up = Update(database, def_path=path, flag=flag)
+    up.get_data(select_old_data)
+    try:
+        up.update_links()
+        up.get_folders()
+        # up.move_files()
+    except Exception as e:
+        print "Error : %s" % e
+        raise SystemExit
